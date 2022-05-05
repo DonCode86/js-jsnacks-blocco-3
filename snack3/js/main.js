29be70dd602b7cd927e5f1d37fd5e37df85b1f4b
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-05 16:35:34 +0200

    snack 3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,6 +1,6 @@
 const array = [1, 3, 6, 10, 11, 13, 15, 17, 18, 19, 20]
-const containeRed = document.querySelectorAll("container-red")
-const containerGreen = document.querySelectorAll("container-green")
+const containeRed = document.querySelector(".container-red")
+const containerGreen = document.querySelector(".container-green")
 for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 == 0) {
         containerGreen.innerText += array[i] + " "
